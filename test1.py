74e17a47786aae0ec4041e51dbd0ea1f37005246
Aryan Verma <aryanverma@Aryans-MacBook-Pro.local>
2026-08-23 04:16:50 +0530

    first version of the code

---
+++ test1.py
@@ -0,0 +1,18 @@
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+c = input("enter operation: ")
+
+if(c == "add" or c == "+"):
+    print(a + b)
+
+elif(c == "subtract" or c == "-"):
+    print(a - b)
+
+elif(c == "divide" or c == "/"):
+    print(a / b)
+
+elif(c == "multiply" or c == "*"):
+    print(a * b)
+
+else:
+    print("Invalid operation")
