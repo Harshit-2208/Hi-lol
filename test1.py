9acfc2ac66e2a2de5d588830ddb13a59b0282919
Harshit-2208 <getyourselfajob81@gmail.com>
2026-09-07 15:13:35 +0530

    hell

--- test1.py
+++ test1.py
@@ -2,11 +2,14 @@ a = int(input("enter first number: "))
 b = int(input("enter second number: "))
 c = input("enter operation: ")
 
-if(c == "add" or c == "+"):
+if(c == "add" or c == "+" or  c == "Add"):
     print(a + b)
 
-elif(c == "subtract" or c == "-"):
+elif(c == "subtract" or c == "-" or c == "Subtract"):
     print(a - b)
 
-elif(c == "divide" or c == "/"):
+elif(c == "divide" or c == "/" or c == "Divide"):
     print(a / b)
+
+elif(c== "Multiply" or c == "*" or c == "Multiply"):
+    print(a * b)
